cfd75fa23c18c2590aae525b7d344896870f6640
joaoveronezi <veronezijoao01@gmail.com>
2020-04-24 15:45:16 -0300

    Adicionando Header, Navbar e Footer

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/sass/base/index.scss';
+import Header from './Header';
+import Footer from './Footer';
+
+const App = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../../assets/images/logo.png'
+import '../../assets/sass/base/index.scss'
+
+const NavBar = () => {
+    return (
+        <div>
+            <img src={Logo} className="logo"></img>
+        </div>
+    )
+}
+
+export default NavBar;
+
+
+
+
