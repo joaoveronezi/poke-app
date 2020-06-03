21fac32de0be8892ddca0a931ed6b30fb8f1797b
joaoveronezi <veronezijoao01@gmail.com>
2020-06-03 13:37:07 -0300

    redux devtools enable

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./reducers";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(
+  reducers,
+  compose(applyMiddleware(sagaMiddleware), reduxDevTools ? reduxDevTools : null)
+);
 
 sagaMiddleware.run(rootSaga);
 export default store;
